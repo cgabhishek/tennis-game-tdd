9fe8f06f69cce2304eb2e9d7958006befe7ecf77
Abhishek Bhardwaj <abhishek.hcst14@gmail.com>
2021-03-19 21:42:09 +0530

    code refactor

--- src/com/company/TennisGameTest.java
+++ src/com/company/TennisGameTest.java
@@ -3,48 +3,55 @@ package com.company;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ * @author abhishek
+ */
+
 public class TennisGameTest {
+
+    TennisGame tennisGame = new TennisGame();
+
     @Test
-    public void test0_0(){
-        TennisGame tennisGame = new TennisGame();
-        Assertions.assertEquals("0-0",tennisGame.score());
+    public void test0_0() {
+        resultAssert("0-0");
 
     }
 
     @Test
-    public void test15_0(){
-        TennisGame tennisGame = new TennisGame();
-        tennisGame.firstPlayerScore();
-        Assertions.assertEquals("15-0",tennisGame.score());
+    public void test15_0() {
+        playerScore(1);
+        resultAssert("15-0");
 
     }
 
     @Test
-    public void test30_0(){
-        TennisGame tennisGame = new TennisGame();
-        tennisGame.firstPlayerScore();
-        tennisGame.firstPlayerScore();
-        Assertions.assertEquals("30-0",tennisGame.score());
+    public void test30_0() {
+        playerScore(2);
+        resultAssert("30-0");
+        ;
 
     }
 
     @Test
-    public void test40_0(){
-        TennisGame tennisGame = new TennisGame();
-        tennisGame.firstPlayerScore();
-        tennisGame.firstPlayerScore();
-        tennisGame.firstPlayerScore();
-        Assertions.assertEquals("40-0",tennisGame.score());
+    public void test40_0() {
+        playerScore(3);
+        resultAssert("40-0");
 
     }
-    @Test
-    public void testWin_0(){
-        TennisGame tennisGame = new TennisGame();
-        tennisGame.firstPlayerScore();
-        tennisGame.firstPlayerScore();
-        tennisGame.firstPlayerScore();
-        tennisGame.firstPlayerScore();
-        Assertions.assertEquals("Win game",tennisGame.score());
 
+    @Test
+    public void testWin_0() {
+        playerScore(4);
+        resultAssert("Win game");
+    }
+
+    private void resultAssert(String expectedResults) {
+        Assertions.assertEquals(expectedResults, tennisGame.score());
+    }
+
+    private void playerScore(int scores) {
+        for (int i = 0; i < scores; i++) {
+            tennisGame.firstPlayerScore();
+        }
     }
 }
