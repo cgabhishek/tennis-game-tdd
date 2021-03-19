f3556899e849895d0868dac63023a90b4bc58a68
Abhishek Bhardwaj <abhishek.hcst14@gmail.com>
2021-03-19 21:02:37 +0530

    first test pass

--- src/com/company/TennisGame.java
+++ src/com/company/TennisGame.java
@@ -1,4 +1,7 @@
 package com.company;
 
 public class TennisGame {
+    public String score() {
+        return "0-0";
+    }
 }
--- src/com/company/TennisGameTest.java
+++ src/com/company/TennisGameTest.java
@@ -1,10 +1,13 @@
 package com.company;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TennisGameTest {
     @Test
     public void test0_0(){
+        TennisGame tennisGame = new TennisGame();
+        Assertions.assertEquals("0-0",tennisGame.score());
 
     }
 }
